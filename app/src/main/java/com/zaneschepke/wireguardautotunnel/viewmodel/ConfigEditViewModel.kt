6788b05fa06952514118cc8d2f4560017dd6a12e
zaneschepke <dev@zaneschepke.com>
2026-06-08 01:57:38 -0400

    fix: globals ui state bug while tunnel active

--- app/src/main/java/com/zaneschepke/wireguardautotunnel/viewmodel/ConfigEditViewModel.kt
+++ app/src/main/java/com/zaneschepke/wireguardautotunnel/viewmodel/ConfigEditViewModel.kt
@@ -93,7 +93,6 @@ class ConfigEditViewModel(
                                 tunnel = tunnel,
                                 tunnels = tunnels,
                                 isRunning = isRunning,
-                                globalSettings = globalSettings,
                             )
                         }
                     }
